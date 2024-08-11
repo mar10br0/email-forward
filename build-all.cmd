c4e29352c1259dacb23374ead1d1f8edb7028bdb
mar10br0 <m@rcodebru.in>
2024-08-12 02:37:28 +1000

    Fix cmd-files calling each other

--- build-all.cmd
+++ build-all.cmd
@@ -6,8 +6,4 @@ go build -o forward.exe cmd\forward\main.go
 set GOOS=linux
 go build -o lambda cmd\lambda\main.go
 
-set AWS_REGION=us-west-2
-set AWS_ACCESS_KEY=$TOKENGEN_AWS_ACCESS_KEY
-set AWS_SECRET_KEY=$TOKENGEN_AWS_SECRET_KEY
-set LAMBDA_BUCKET=email-forward-lambda-package
-token-gen.exe
+./new-token.cmd
